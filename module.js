b180bbb722cad6319321ad95955e4316d96371d9
Justin B. Watson <justin@watsonnetworks.com>
2021-04-13 12:18:28 -0500

    Update module.js

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 function broadcast() {
     koi.sendMessage("Be sure to follow TWG2022 on Twitter https://twitter.com/TWG2022");
 
-    setTimeout(broadcast, (1 * 30) * 1000);
+    setTimeout(broadcast, (5 * 60) * 1000);
 }
 
 broadcast();
